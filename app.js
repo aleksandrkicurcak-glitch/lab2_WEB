5f7db0bbd023271f180e86b10a1aa08620297308
aleksandrkicurcak-glitch <aleksandrkicurcak@gmail.com>
2026-06-18 09:38:59 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
-// app.js
+
 document.addEventListener('DOMContentLoaded', () => {
-    // Ініціалізуємо наш додаток, передаючи екземпляри Моделі та View у Контролер
     const app = new BlogController(new BlogModel(), new BlogView());
 });
