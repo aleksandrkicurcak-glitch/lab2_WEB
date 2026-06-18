cdb100d32aa9a792a14ff13f0046ea802819909a
aleksandrkicurcak-glitch <aleksandrkicurcak@gmail.com>
2026-06-18 09:40:39 +0300

    Update profile.js

--- profile.js
+++ profile.js
@@ -1,4 +1,4 @@
-// profile.js
+
 document.addEventListener('DOMContentLoaded', () => {
     const currentUser = JSON.parse(localStorage.getItem('devblog_current_user'));
     
@@ -7,7 +7,7 @@ document.addEventListener('DOMContentLoaded', () => {
         return window.location.href = 'login.html';
     }
 
-    // Оновлюємо візуальну частину
+
     const profileHeader = document.querySelector('.bg-indigo-50 h2');
     const profileInitials = document.querySelector('.bg-indigo-50 .w-24');
     const tdElements = document.querySelectorAll('tbody td:nth-child(2)');
@@ -24,13 +24,13 @@ document.addEventListener('DOMContentLoaded', () => {
         tdElements[3].textContent = currentUser.dob;
     }
 
-    renderProfileInfo(); // Первинне заповнення
+    renderProfileInfo(); 
 
-    // Логіка редагування
+
     let isEditing = false;
     editBtn.addEventListener('click', () => {
         if (!isEditing) {
-            // Перетворюємо поля Ім'я та Дата народження на інпути
+
             tdElements[0].innerHTML = `<input type="text" id="edit-name" value="${currentUser.name}" class="border rounded px-2 py-1 w-full text-sm">`;
             tdElements[3].innerHTML = `<input type="date" id="edit-dob" value="${currentUser.dob}" class="border rounded px-2 py-1 w-full text-sm">`;
             
@@ -38,14 +38,14 @@ document.addEventListener('DOMContentLoaded', () => {
             editBtn.classList.replace('bg-indigo-600', 'bg-green-600');
             isEditing = true;
         } else {
-            // Зберігаємо зміни
+   
             currentUser.name = document.getElementById('edit-name').value;
             currentUser.dob = document.getElementById('edit-dob').value;
             
-            // Оновлюємо поточного юзера
+
             localStorage.setItem('devblog_current_user', JSON.stringify(currentUser));
             
-            // Оновлюємо його в загальній базі користувачів
+
             const users = JSON.parse(localStorage.getItem('devblog_users')) || [];
             const userIndex = users.findIndex(u => u.email === currentUser.email);
             if (userIndex !== -1) {
@@ -53,13 +53,13 @@ document.addEventListener('DOMContentLoaded', () => {
                 localStorage.setItem('devblog_users', JSON.stringify(users));
             }
 
-            renderProfileInfo(); // Повертаємо текстовий вигляд
+            renderProfileInfo(); 
             
             editBtn.textContent = 'Редагувати профіль';
             editBtn.classList.replace('bg-green-600', 'bg-indigo-600');
             isEditing = false;
             
-            // Оновлюємо ім'я в навігації
+    
             document.querySelector('nav span').textContent = `Привіт, ${currentUser.name}!`;
         }
     });
